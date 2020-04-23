709de2ed639d95ef140cd84f10f1d83f55a49695
Mateusz Bełczowski <mateusz.belczowski@gmail.com>
2020-04-23 19:21:06 +0200

    Zapisywanie odpowiedzi w bazie

--- game.py
+++ game.py
@@ -1,6 +1,14 @@
-from db import get_random_lyrics, get_song_by_id
+from db import get_random_lyrics, get_song_by_id, add_quiz, add_answer, summarize_quiz
 
 class GuessThatSongQuiz:
+
+    def __init__(self):
+        username = input("What's your username? ->")
+        total_questions = input("How many questions do you want? ->")
+        quiz_id = add_quiz(username, total_questions)
+        self.quiz_id = quiz_id
+        self.total_questions = total_questions
+
 
     def ask_question(self):
         """
@@ -17,9 +25,12 @@ class GuessThatSongQuiz:
         else:
             print(f"Nope, it was {correct_artist} with the song {title}")
 
+        add_answer(song_id, self.quiz_id, answer, lyrics)
+
 
 
 
 
 quiz = GuessThatSongQuiz()
 quiz.ask_question()
+print(summarize_quiz(quiz.quiz_id))
